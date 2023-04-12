8650a35ad7b2304a12983b9546c23c38da2fd2ba
Ilya-Abramov00 <128179332+Ilya-Abramov00@users.noreply.github.com>
2023-04-12 15:52:52 +0300

    Update complex_test.cpp

--- TestTask/libs/complex/test/complex_test.cpp
+++ TestTask/libs/complex/test/complex_test.cpp
@@ -11,21 +11,21 @@ TEST(complex_test, inicializ) {
     complex<int >a(1);
    ASSERT_TRUE(a.Re()==1);
       ASSERT_TRUE(a.Im()==0);
-      cout<<a;
+    
 }
 
 TEST(complex_test, read) {
     complex<int >a(1);
    ASSERT_TRUE(a.Re()==1);
       ASSERT_TRUE(a.Im()==0);
-        cout<<a;
+      
 }
 
 TEST(complex_test, write) {
     complex<int >a(1);
    ASSERT_TRUE(a.Re(2)==2);
       ASSERT_TRUE(a.Im(4)==4);
-       cout<<a;
+     
 }
 
 TEST(complex_test, abs) {
@@ -37,21 +37,18 @@ TEST(complex_test, abs) {
 TEST(complex_test, Arg) {
     complex<float >a(-2,0);
 
-   cout<<a.Arg()<<endl;
    }
 
 TEST(complex_test, Arg1)
   {
     complex<float >b(0,0);
 
-        cout<<b.Arg()<<endl;
         }
 
 TEST(complex_test, ArgDeg)
  {
     complex<float >a(-2,2);
 
-      cout<<a.ArgDeg()<<endl;
 
 }
 
@@ -66,7 +63,7 @@ TEST(complex_test, conj) {
 TEST(complex_test, CreateComplexFromPhaseDeg) {
 
     auto a=complex<float>::Factory::CreateComplexFromPhaseDeg(PI);
-  cout<<a;
+  
   ASSERT_TRUE(a.Re()==-1);
       ASSERT_TRUE(a.Im()<0.01);
 }
