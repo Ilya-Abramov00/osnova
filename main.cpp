13ca41502ec9990d9f726b5aea2ef12a6aa2acb8
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-04-14 18:19:14 +0300

    12

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
 
 using namespace std;
-#include "TestTask/libs/signal/include/signal/signal.h"
+#include "signal/signal.h"
 #include <iostream>
 #include <vector>
 
 int main() {
-vector<complex<float>> a={{1,2},{1,4}};
-auto b= downsample(a,4);
-cout<<b.at(0);
+    string ad = "/home/ilya/Загрузки/am_sound.dat";
+    string ad1 = "/home/ilya/Загрузки/pere_am_sound.dat";
+
+    string ud = "/home/ilya/Загрузки/file1EuropaPlus.bin";
+    string ud1 = "/home/ilya/Загрузки/pere_file1EuropaPlus.bin";
+
+    auto c=FileManeger<int>::SavedSignal(ad);
+    auto c1=DemodulatorAM::demodul(c);
+
+
+    FileManeger<float>::SaveSignal(ad1,c1);
+
+
+
+    auto a=FileManeger<float>::SavedSignal(ud);
+
+
+    auto a1=DemodulatorFM::demodul(a);
+
+
+    auto a2=Recursiv::filsr(a1,8);
+
+
+    auto a3= downsample(a2,25);
+
+
+    FileManeger<float>::SaveSignal(ud1,a3);
+
 
 }
