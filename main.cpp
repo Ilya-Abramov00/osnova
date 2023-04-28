eafaaed5ee4eec8124179be82110bd635583fdb0
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-04-28 22:49:59 +0300

    23

--- main.cpp
+++ main.cpp
@@ -2,65 +2,33 @@
 using namespace std;
 #include "signal/signal.h"
 #include <iostream>
-#include "AudioFile.h"
+#include <math.h>
+
 #include <vector>
 
 int main() {
-    string ad = "/home/ilya/Загрузки/am_sound.dat";
-    string ad1 = "/home/ilya/Загрузки/pere_am_sound.dat";
+    float fas=0;
+    float A=1;
+    float f=0.1;
+    float fas1=fas*180/PI;
+    float div=0.01;
 
-    string ud = "/home/ilya/Загрузки/file1EuropaPlus.bin";
-    string ud1 = "/home/ilya/Загрузки/pere_file1EuropaPlus.bin";
+    vector<float> sig(0,0);
 
-    auto c=FileManeger<int>::SavedSignal(ad);
-   auto c1=DemodulatorAM::demodul(c);
-auto c2=normirovca(c1);
-
-   FileManeger<float>::SaveSignal(ad1,c1);
-
-
-
-    auto a=FileManeger<float>::SavedSignal(ud);
-
-
-    auto a1=DemodulatorFM::demodul(a);
-
-
-    auto a2=Recursiv::filsr(a1,8);
-
-
-    auto a3= downsample(a2,25);
-
-
-   FileManeger<float>::SaveSignal(ud1,a3);
-
-
-    string ad3 = "/home/ilya/Загрузки/examplAM";
-AudioFile<float> audioFile;
-    auto m=c2.size();
-    audioFile.setSampleRate(16000);
-    audioFile.setAudioBufferSize(1,m);
-
-    audioFile.setBitDepth(32);
-    audioFile.setNumChannels(1);
-    for(int i=0;i<m;i++)
+    for (int k=0; k<=100; k++)
     {
-        audioFile.samples[0][i]=(c2[i]);
+        sig.emplace_back(sin(f*k+fas1));
     }
-audioFile.save(ad3,AudioFileFormat::Wave);
+    auto m=sig.size();
 
-    string ad4 = "/home/ilya/Загрузки/examplFM";
-    AudioFile<float> audioFile1;
-    auto n=a3.size();
-    audioFile1.setSampleRate(20000);
-    audioFile1.setAudioBufferSize(1,n);
+    for (int i = 0; i <= m - 1; i++ ) {
+        cout<<sig.at(i)<<" ";
+    }
 
-    audioFile1.setBitDepth(32);
-    audioFile1.setNumChannels(1);
-    for(int i=0;i<n;i++)
+    for (int k=0; k<=100; k++)
     {
-        audioFile1.samples[0][i]=(a3[i]);
+        sig.emplace_back(sin((sin(div*k)*f)*k+fas1));
     }
-    audioFile1.save(ad4,AudioFileFormat::Wave);
 
+    return 1;
 }
