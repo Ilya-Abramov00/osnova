dc1c061f289f798ce8fdf851e70c7abba66d06ef
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-05-02 09:55:57 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,33 +1,31 @@
 
-using namespace std;
-#include "signal/signal.h"
+
+#include <fstream>
 #include <iostream>
-#include <math.h>
+#include <queue>
+#include <thread>
+#include <chrono>
 
-#include <vector>
+int main()
+{
 
-int main() {
-    float fas=0;
-    float A=1;
-    float f=0.1;
-    float fas1=fas*180/PI;
-    float div=0.01;
-
-    vector<float> sig(0,0);
-
-    for (int k=0; k<=100; k++)
-    {
-        sig.emplace_back(sin(f*k+fas1));
-    }
-    auto m=sig.size();
-
-    for (int i = 0; i <= m - 1; i++ ) {
-        cout<<sig.at(i)<<" ";
-    }
-
-    for (int k=0; k<=100; k++)
-    {
-        sig.emplace_back(sin((sin(div*k)*f)*k+fas1));
+    std::queue <char> ad;
+    std::ifstream fin;
+    fin.open("/home/ilya/zad2.txt",  std::ios_base::in);
+    std::thread qw;
+    if (!fin.is_open()) { std::cout << "Файл не может быть открыт\n"; }
+    else {
+        char a;
+        char &b=a;
+        while (fin.get(a) )
+        {
+            ad.push(a);
+            std::cout<<a;
+std::this_thread::sleep_for(std::chrono::microseconds(200));
+        }
+        std::cout<<ad.size();
+        std::cout << "Файл открыт\n";
+        fin.close();
     }
 
     return 1;
