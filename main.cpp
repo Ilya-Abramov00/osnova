67acb698c3fde0e87064bb7b1280c9d819735470
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-05-03 22:03:34 +0300

    1

--- main.cpp
+++ main.cpp
@@ -5,28 +5,56 @@
 #include <queue>
 #include <thread>
 #include <chrono>
+#include <mutex>
+#include <condition_variable>
+
+std::mutex mtx;
+std::condition_variable cv;
 
 int main()
 {
+    int time=20;
+    std::queue <char> ad1;
 
-    std::queue <char> ad;
-    std::ifstream fin;
-    fin.open("/home/ilya/zad2.txt",  std::ios_base::in);
-    std::thread qw;
-    if (!fin.is_open()) { std::cout << "Файл не может быть открыт\n"; }
-    else {
-        char a;
-        char &b=a;
-        while (fin.get(a) )
+
+
+    std::thread qw1([&ad1,&time]
+    {
+        char a = 1;//иммитация данных
+
+        for(int i=0; i!=1024 ; i++)
         {
-            ad.push(a);
-            std::cout<<a;
-std::this_thread::sleep_for(std::chrono::microseconds(200));
-        }
-        std::cout<<ad.size();
-        std::cout << "Файл открыт\n";
-        fin.close();
-    }
+            std::unique_lock<std::mutex> mtx_0(mtx);
+            for (int j = 0; j != 1024 * 8; j++)
+            {
 
+                ad1.push(a);
+            }
+            mtx.unlock();
+            std::this_thread::sleep_for(std::chrono::microseconds(time));
+        }
+    });
+
+
+
+    std::cout <<"\n"<< ad1.size()<<"\n";
+
+        std::ofstream fout("/home/ilya/zad2.txt", std::ios_base::trunc);
+        if (!fout.is_open()) { std::cout << "Файл не может быть открыт\n"; }
+        else
+        {
+
+            int m = ad1.size();
+            while(ad1.empty())
+            {
+                auto p = ad1.front();
+                fout.write((char *) &p, sizeof(p));
+                ad1.pop();
+            }
+        fout.close();
+
+        }
+
+qw1.join();
     return 1;
 }
