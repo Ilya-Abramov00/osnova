f769a30c284fcbeb3798cf5235395cffb79a6292
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-05-05 17:45:34 +0300

    1

--- main.cpp
+++ main.cpp
@@ -16,64 +16,89 @@ bool ret() { return var;}
 bool stop= true;//для остановки первого потока
 int k=0;
 
-bool stops(){
-    if(k==1024){stop=false;}
+bool stops()
+{
+    if( k==1024 ) { stop=false; }
     return stop;
 };
 
-
-
-
-    void read(std::queue <char>& buf1 , int time1)
+int  write_buf( char *buf_0, int sdvig )//иммитация записи данных
+{
+    char a='q';
+    int data_size=1024;
+    for (int j = 0; j != data_size ; j++)
     {
-        char a = 'q';//иммитация записи данных
-        while (stops())
-        {
-            std::unique_lock<std::mutex> mtx_0(mtx);
-            while(buf1.size()!=0)
-            {
-                    cv.wait(mtx_0);//пока он занят, запись запрещена
-            }
+        *(buf_0+sdvig +j)=a ;
+        std::this_thread::sleep_for(std::chrono::microseconds(20));
+    }
+return data_size;
+}
 
-            for (int j = 0; j != 1024; j++) { buf1.push(a); }
+
+    void write(std::queue <char*> & queue , char *buf_0 )
+    {
+
+        while ( stops() )
+        {
+            int sdvig=1024*k;//уже записано столько мб
+
+            std::unique_lock<std::mutex> mtx_0(mtx);
+
+            int data_size= write_buf( buf_0, sdvig);
+
+           auto buf_begin=buf_0+sdvig;
+           auto buf_end=buf_begin+data_size;
+
+           queue.push(buf_begin ) ;
+           queue.push(buf_end );
 
             mtx_0.unlock();
 
-            std::this_thread::sleep_for(std::chrono::microseconds(time1));
             k++;
         }
         var = false;
     }
 
-void write(std::queue <char>& buf1 )
+void read( std::queue <char *> & queue ,char *buf_0 )
 {
-    std::ofstream fout("/home/ilya/zad2.txt", std::ios_base::app | std::ios_base::out);
-    while (ret())
-    {
-        while (buf1.size() != 0)
-        {
-            auto p = buf1.front();
-            fout.write((char *) &p, 1);
-            buf1.pop();
-        }
-        cv.notify_one();
 
+    std::ofstream fout("/home/ilya/zad2.txt", std::ios_base::app | std::ios_base::out);
+
+
+    while ( ret() )
+    {
+
+        while ( queue.empty() == 0)
+        {
+
+            auto p_begin = queue.front();
+            queue.pop();
+            auto p_end = queue.front();
+            queue.pop();
+
+            for( ; p_begin != p_end ; p_begin++ )
+            {
+               char data=*(p_begin);
+               fout.write((char *) &data, 1);
+            }
+        }
     }
     fout.close();
 }
 
 int main()
 {
-        int time=20;
-        std::queue <char> buf;
+        std::queue <char *> queue ;
+        char buf[1024];
+        char* buf_0=buf;
 
-        std::thread q1(read, std::ref (buf) , time );
+        std::thread q1 (write, std::ref (queue) , buf_0);
 
-        std::thread q2(write, std::ref (buf) );
+        std::thread q2 (read , std::ref (queue) , buf_0) ;
 
         q1.join();
         q2.join();
 
-    if( buf.size()==0){ std::cout<<" \n Очередь пуста";}
+    if( queue.empty() ){ std::cout<<" \n Очередь пуста";}
     return 1;
 }
