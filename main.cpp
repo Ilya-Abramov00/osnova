37600abb34ce0610cbe9547677184194a2fd776c
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-05-21 20:11:40 +0300

    ooп

--- main.cpp
+++ main.cpp
@@ -9,20 +9,20 @@ int main()
     char * buf_0=new char[1024*1024*256];
 
 
- /*
+ // /*
     Write q1(queue,buf_0);
     Read q2( queue,buf_0);
     q1.CreateThr();
     q2.CreateThr();
- */
+ // */
 
 
-  // /*
+   /*
     std::thread q1 (write, std::ref (queue) , buf_0);
     std::thread q2 (read , std::ref (queue) , buf_0) ;
     q1.join();
     q2.join();
-  // */
+   */
 
     std::cout<<" \n \n работа закончена";
     delete [] buf_0;
