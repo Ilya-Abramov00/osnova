c32b94aee09a3ea1b730b271c6049451466b9b9e
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-05-29 17:46:45 +0300

    12

--- main.cpp
+++ main.cpp
@@ -3,22 +3,6 @@
 
 int main()
 {
-    auto start=std::chrono::high_resolution_clock::now();
 
-    std::queue <Msg> queue ;
-    char * buf_0=new char[1024*1024*256];
-
-
-    Write_thread q1(queue,buf_0);
-    Read_thread q2( queue,buf_0,"/home/ilya/zad2.txt" );
-    q1.CreateThr();
-    q2.CreateThr();
-
-
-    delete [] buf_0;
-
-    auto end=std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float> duration=end-start;
-    std::cout<<"\n время заняло= "<<duration.count()<<"\n";
     return 1;
 }
