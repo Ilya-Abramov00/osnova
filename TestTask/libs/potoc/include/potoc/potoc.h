c6e07848a1d00b16d6f5afe94cc33184565a777a
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-05-29 13:50:59 +0300

    12

--- TestTask/libs/potoc/include/potoc/potoc.h
+++ TestTask/libs/potoc/include/potoc/potoc.h
@@ -12,28 +12,24 @@
 
 std::mutex mtx;
 std::condition_variable cv;
+
 int k=0;
-
-bool var= true;//для остановки второго потока
-
+bool var= true;//для остановки потока чтения
 bool ret() { return var;}
-
-bool stop= true;//для остановки первого потока
-
+bool stop= true;//для остановки  потока записи
 bool stops()
 {
     if( k==256 ) { stop=false; }
     return stop;
 };
 
-
-int  write_buf( char *buf_0, int sdvig );
-
 struct Msg//сообщение
 {
     char * begin;
     char * end;
 };
+
+int  write_buf( char *buf_0, int sdvig );//функция реализуящая запись
 
 
 void write(std::queue <Msg> & queue , char *buf_0 );
@@ -78,6 +74,7 @@ private:
     {
         read( p->queue ,  p->buf_0, p->ptr);
     }
+
     std::queue <Msg> & queue;
     char *&buf_0;
     std::string ptr;
