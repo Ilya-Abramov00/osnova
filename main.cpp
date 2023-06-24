63145667a75e0272cd101821ae74f3d665c1f294
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-06-24 22:28:09 +0300

    1

--- main.cpp
+++ main.cpp
@@ -8,14 +8,7 @@
 
 int main()
 {
-    std::queue <Msg> queue ;
-    std::vector<char> a;
-    a.reserve(1024*1024*256);
-    std::vector <char> :: iterator it=a.begin();
-    std::mutex mtx;
 
-    Protocol q(queue, it, mtx, "/home/ilya/potoc.txt",0.2,0);
-    q.start();
 
     return 0;
 }
