f50025d9eafa5f34d1b88f1a0af67d9b8ee46e76
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-06-27 16:44:13 +0300

    1

--- TestTask/libs/Data_package/test/data_package_test.cpp
+++ TestTask/libs/Data_package/test/data_package_test.cpp
@@ -10,7 +10,7 @@ TEST(data_package, 1) {
     std::string str_3="/home/ilya/Загрузки/file_3.txt";
     std::string str_4="/home/ilya/Загрузки/file_4.txt";
 
-    int N=10;
+    int N=100;
 
     geniration_string( N, str_1);
 
