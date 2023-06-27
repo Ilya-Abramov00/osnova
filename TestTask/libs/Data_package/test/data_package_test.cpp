23093eeda2cced17ec24ce8d340f0799c7029029
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-06-27 12:41:23 +0300

    1

--- TestTask/libs/Data_package/test/data_package_test.cpp
+++ TestTask/libs/Data_package/test/data_package_test.cpp
@@ -3,6 +3,22 @@
 #include "data_package/data_package.h"
 
 TEST(data_package, 1) {
- ASSERT_TRUE(2==2);
+
+    std::string str_1="/home/ilya/Загрузки/file_1.txt";
+    std::string str_2="/home/ilya/Загрузки/file_2.txt";
+    std::string str_3="/home/ilya/Загрузки/file_3.txt";
+    std::string str_4="/home/ilya/Загрузки/file_4.txt";
+
+    int N=100;
+
+    geniration_string( N, str_1);
+
+    Socket::Packaging_Socket(N,str_1,str_2);
+
+    Socket::Random_Socket(str_2,str_3);
+
+    Socket::Sent_Socket (str_3,str_4);
+
+
 }
 
