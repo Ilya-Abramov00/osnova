fa9e794d2f3c459982bd6fcc7c16655f65c26f3a
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-06-28 18:55:28 +0300

    1

--- main.cpp
+++ main.cpp
@@ -11,12 +11,14 @@ int main()
 
 
 uint16_t magic;
-uint8_t * magBuf=reinterpret_cast<uint8_t * >(&magic);
+uint8_t * magBuf=reinterpret_cast<uint8_t*>(&magic);
 
-magBuf[0]=0x00;
-magBuf[1]=0x00;
+magic=0XFFFF;
 
-std::cout<<magic;
+    std::cout<<"[0]="<<std::hex<<+magBuf[0]<<"\n";
+    std::cout<<"[1]="<<std::hex<<+magBuf[1] <<std::endl;
+
+    std::cout<< std::dec<<"magic=" <<magic<<"\n";
 
 std::string str_1="/home/ilya/Загрузки/file_1.txt";
 
