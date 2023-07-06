1c914dc7be1f7691753d8f04d4ce22912720c22e
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-07-06 14:58:29 +0300

    1

---
+++ TestTask/libs/DataPackage/DataPackageCommon/test/WriteReadString.cpp
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+
+#include "DataPackageCommon/CommonPackage.h"
+
+using namespace std;
+
+TEST(WriteReadString, writestring_readfullfile)
+{
+	std::string str_1 = "string_test/file_1.txt";
+
+	string data_1("absegegd");
+
+	write_string(data_1, str_1);
+
+	auto data_2 = readfullfile(str_1);
+
+	ASSERT_TRUE(data_1.size() == data_2.size());
+	for(int i = 0; i != data_1.size(); i++) {
+		ASSERT_TRUE(data_1[i] == data_2[i]);
+	}
+}
+TEST(WriteReadString, writestring_readstring)
+{
+	std::string str_1 = "string_test/file_1.txt";
+
+	string data_1("absegegd");
+
+	write_string(data_1, str_1);
+
+	auto data_2 = read_string(str_1);
+
+	ASSERT_TRUE(data_1 == data_2);
+}
+TEST(WriteReadString, readstring)
+{
+	std::string str_1 = "string_test/file_1.txt";
+
+	auto data_test = geniration_string(33, 100, str_1);
+
+	write_string(data_test, str_1);
+
+	auto data = read_string(str_1);
+
+	ASSERT_TRUE(data_test== data);
+}
---
+++ TestTask/libs/DataPackage/DataPackageWrite/include/DataPackageWrite/WritePackage.h
@@ -0,0 +1,103 @@
+#ifndef WRITE_PACKAGE_H
+#define WRITE_PACKAGE_H
+
+
+
+#include <algorithm>
+
+#include <iostream>
+#include <list>
+#include <vector>
+#include <fstream>
+#include <random>
+
+
+#include "DataPackageCommon/CommonPackage.h"
+
+template <size_t T>
+class FilePackageWrite {
+	public:
+	FilePackageWrite()
+	{}
+
+	void get_File_Messeges(std::string const& namefile)
+	{
+		uint16_t id     = 0;
+		auto bufferfile = readfullfile(namefile);
+		std::string buffer;
+		buffer.reserve(T);
+
+		for(int i = 1; i != bufferfile.size() + 1; i++) {
+			buffer.push_back(bufferfile.at(i - 1));
+
+			if(!(i % T)) {
+				Messeges_data.emplace_back(Msg<T>(buffer, ++id));
+				buffer.clear();
+			}
+		}
+		if(buffer.size() != 0) {
+			Messeges_data.emplace_back(Msg<T>(buffer, ++id));
+			buffer.clear();
+		}
+	}
+
+	void shuffle_write_messeges(std::string const& namefile)
+	{
+		std::vector<std::reference_wrapper<const Msg<T>>> v(Messeges_data.cbegin(), Messeges_data.cend());
+		std::shuffle(v.begin(), v.end(), gen);
+
+		std::ofstream file;
+		file.open(namefile, std::ios::trunc);
+		if(!file.is_open()) {
+			throw MyException("файл не открылся");
+		}
+
+		for(int i = 0; i != v.size(); i++) {
+			write_Msg_file(v.at(i).get(), file);
+		}
+
+		file.close();
+	}
+
+	void write_messeges(std::string& filename)
+	{
+		std::ofstream file;
+		file.open(filename, std::ios::trunc);
+		if(!file.is_open()) {
+			throw MyException("файл не открылся");
+		}
+
+		for(auto i: Messeges_data) {
+			write_Msg_file(i, file);
+		}
+		file.close();
+	}
+
+	void clear()
+	{
+		Messeges_data.clear();
+	}
+
+	Messeges<T> const& get_Messeges()
+	{
+		return Messeges_data;
+	}
+
+private:
+
+	void write_Msg_file(const Msg<T>& msg, std::ofstream& file)
+	{
+		file.write((char*)&Head, sizeof(Head));
+		file.write((const char*)&(msg.get_id()), sizeof(msg.get_id()));
+		file.write(msg.get_data(), T);
+		file.write((char*)&Tail, sizeof(Tail));
+	}
+
+	uint16_t Head = 0xBABA;
+	uint16_t Tail = 0xDEDA;
+	Messeges<T> Messeges_data;
+};
+
+
+
+#endif
